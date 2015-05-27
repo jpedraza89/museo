74594212f2dce1febb59e18dc22b5271bcdfaba6
angeloLuna <angel.luna.najera@gmail.com>
2015-05-27 17:53:59 -0500

    Merge with API branch

--- app/src/main/java/com/example/angelo/museo/MuseoActivity.java
+++ app/src/main/java/com/example/angelo/museo/MuseoActivity.java
@@ -1,21 +1,17 @@
 package com.example.angelo.museo;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.example.angelo.museo.ui.fragments.FirstFragment;
 import com.example.angelo.museo.ui.fragments.SecondFragment;
 import com.example.angelo.museo.ui.fragments.ThirdFragment;
 
-public class MuseoActivity extends ActionBarActivity {
+public class MuseoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
