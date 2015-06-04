e2959d02204014c1b6f880f63218e137858cc6e7
angeloLuna <angel.luna.najera@gmail.com>
2015-06-03 19:08:06 -0500

    removed ViewPager from MuseoActivity

--- app/src/main/java/com/example/angelo/museo/MuseoActivity.java
+++ app/src/main/java/com/example/angelo/museo/MuseoActivity.java
@@ -18,38 +18,6 @@ public class MuseoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_museo);
-
-        ViewPager pager = (ViewPager) findViewById(R.id.viewPager);
-        pager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
     }
 
-    private class MyPagerAdapter extends FragmentPagerAdapter {
-        public MyPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int pos) {
-            switch (pos) {
-
-                case 0:
-                    return FirstFragment.newInstance("Evento");
-                case 1:
-                    return SecondFragment.newInstance("SecondFragment, Instance 1");
-                case 2:
-                    return ThirdFragment.newInstance("ThirdFragment, Instance 1");
-                case 3:
-                    return ThirdFragment.newInstance("ThirdFragment, Instance 2");
-                case 4:
-                    return ThirdFragment.newInstance("ThirdFragment, Instance 3");
-                default:
-                    return ThirdFragment.newInstance("ThirdFragment, Default");
-            }
-        }
-
-        @Override
-        public int getCount() {
-            return 5;
-        }
-    }
 }
